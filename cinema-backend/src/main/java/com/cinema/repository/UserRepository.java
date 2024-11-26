82b19123398624b0ef3c52c3b1e7fe6e0bab73ef
Agustin Santicchia <santicchiaagus85@gmail.com>
2024-11-26 13:10:31 -0300

    First Commit

---
+++ cinema-backend/src/main/java/com/cinema/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.cinema.repository;
+
+import java.util.Optional;
+
+import com.cinema.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findById(Long id);
+
+    void deleteById(Long id);
+}
---
+++ cinema-backend/src/test/java/com/cinema/controller/UserControllerTest.java
@@ -0,0 +1,7 @@
+package com.cinema.controller;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+public class UserControllerTest {
+}
