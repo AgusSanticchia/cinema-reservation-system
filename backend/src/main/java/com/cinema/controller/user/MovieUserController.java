c38e9cf1f006e9beba6a03095befb86befda1200
Agustin Santicchia <santicchiaagus85@gmail.com>
2024-12-02 21:10:57 -0300

    Movie Reservation Develop

--- backend/src/main/java/com/cinema/controller/user/MovieUserController.java
+++ backend/src/main/java/com/cinema/controller/user/MovieUserController.java
@@ -1,4 +1,38 @@
 package com.cinema.controller.user;
 
+import com.cinema.entity.Movie;
+import com.cinema.service.MovieService;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user/movie")
+@CrossOrigin(origins = "*")
 public class MovieUserController {
+
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        List<Movie> movies = movieService.findAll();
+        return new ResponseEntity<>(movies, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getMovieById(@PathVariable Long id) {
+        Optional<Movie> movie = movieService.findById(id);
+        return movie.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
 }
--- backend/src/main/java/com/cinema/service/MovieService.java
+++ backend/src/main/java/com/cinema/service/MovieService.java
@@ -1,7 +1,35 @@
 package com.cinema.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.cinema.entity.Movie;
+import com.cinema.repository.MovieRepository;
 
 @Service
 public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> findAll() {
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> findById(Long id) {
+        return movieRepository.findById(id);
+    }
+
+    public Movie save(Movie movie) {
+        return movieRepository.save(movie);
+    }
+
+    public void deleteById(Long id) {
+        movieRepository.deleteById(id);
+    }
+
 }
+
